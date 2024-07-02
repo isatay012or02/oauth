5e178982acf7a7453cbae53131028827243acd09
Isatay012or02 <isa.abdr@bk.ru>
2024-07-02 10:42:40 +0500

    first init

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/google/uuid"
+	"gopkg.in/oauth2.v3/errors"
+	"gopkg.in/oauth2.v3/manage"
+	"gopkg.in/oauth2.v3/models"
+	"gopkg.in/oauth2.v3/server"
+	"gopkg.in/oauth2.v3/store"
+	"log"
+	"net/http"
+)
+
+func main() {
+	manager := manage.NewDefaultManager()
+	manager.SetAuthorizeCodeTokenCfg(manage.DefaultAuthorizeCodeTokenCfg)
+
+	manager.MustTokenStorage(store.NewMemoryTokenStore())
+
+	clientStore := store.NewClientStore()
+	manager.MapClientStorage(clientStore)
+
+	srv := server.NewDefaultServer(manager)
+	srv.SetAllowGetAccessRequest(true)
+	srv.SetClientInfoHandler(server.ClientFormHandler)
+	manager.SetRefreshTokenCfg(manage.DefaultRefreshTokenCfg)
+
+	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
+		log.Println("Internal Error:", err.Error())
+		return
+	})
+
+	srv.SetResponseErrorHandler(func(re *errors.Response) {
+		log.Println("Response Error:", re.Error.Error())
+	})
+
+	http.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
+		err := srv.HandleTokenRequest(w, r)
+		if err != nil {
+			return
+		}
+	})
+
+	http.HandleFunc("/credentials", func(w http.ResponseWriter, r *http.Request) {
+		clientId := uuid.New().String()[:8]
+		clientSecret := uuid.New().String()[:8]
+		err := clientStore.Set(clientId, &models.Client{
+			ID:     clientId,
+			Secret: clientSecret,
+			Domain: "http://localhost:9094",
+		})
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"CLIENT_ID": clientId, "CLIENT_SECRET": clientSecret})
+	})
+
+	http.HandleFunc("/protected", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, I'm protected"))
+	})
+	log.Fatal(http.ListenAndServe(":9096", nil))
+}
